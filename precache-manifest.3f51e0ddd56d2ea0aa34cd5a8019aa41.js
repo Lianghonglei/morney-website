ff6ef2893077b5001cf370fc6af99aa25cf2eef2
lei <895641390@qq.com>
2021-02-15 11:23:41 +0800

    deploy

---
+++ precache-manifest.3f51e0ddd56d2ea0aa34cd5a8019aa41.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa3a20e673aee8b73287",
+    "url": "/morney-website/css/app.37c490d2.css"
+  },
+  {
+    "revision": "751ecf12dfac9b026b24f401b1d8b503",
+    "url": "/morney-website/index.html"
+  },
+  {
+    "revision": "aa3a20e673aee8b73287",
+    "url": "/morney-website/js/app.54847eee.js"
+  },
+  {
+    "revision": "09d267b8486168c03b89",
+    "url": "/morney-website/js/chunk-vendors.afbb2b31.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/morney-website/manifest.json"
+  },
+  {
+    "revision": "1965476a7eb9cbfe8d8c78bcafe6785a",
+    "url": "/morney-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/morney-website/robots.txt"
+  }
+]);
